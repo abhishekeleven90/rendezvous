a1b13029c1f7746c97e296d176faf84748d3bbb4
Harinder Sethi <sethi.harinder@gmail.com>
2014-10-20 02:36:37 +0530

    adding attributes, forgot last time

---
+++ game/GeneralHelper.h
@@ -0,0 +1,44 @@
+#include "Headers.h"
+
+using namespace std;
+
+void printGrid() {
+	cout << "Printing grid" << "\n";
+	for (int r = START_GRID_ROW; r <= END_GRID_ROW; r++) {
+		for (int c = START_GRID_COL; c <= END_GRID_COL; c++) {
+			switch (gridChar[r][c]) {
+			case GRASS:
+				cout << " GR";
+				break;
+			case SPAWN:
+				cout << " SP";
+				break;
+			case ANGELS_TEMPLE:
+				cout << " TA";
+				break;
+			case DEMONS_TEMPLE:
+				cout << " TD";
+				break;
+			case H_STUNNER:
+				cout << " HX";
+				break;
+			case H_SILENCER:
+				cout << " HS";
+				break;
+			case H_SNATCHER:
+				cout << " HN";
+				break;
+			case H_LEECHER:
+				cout << " HL";
+				break;
+			case STONE:
+				cout << " ST";
+				break;
+			case TREE:
+				cout << " TR";
+				break;
+			}
+		}
+		cout << "\n";
+	}
+}
---
+++ game/Objects.h
@@ -0,0 +1,72 @@
+#include <string>
+
+char gridChar[END_GRID_ROW - START_GRID_ROW + 2][END_GRID_COL - START_GRID_COL
+		+ 2]; //+1 extra since taking 1-20
+
+enum charCellType {
+	GRASS,
+	SPAWN,
+	ANGELS_TEMPLE,
+	DEMONS_TEMPLE,
+	H_STUNNER,
+	H_SILENCER,
+	H_SNATCHER,
+	H_LEECHER,
+	STONE,
+	TREE
+};
+
+enum Heroes {
+	STUNNER, SILENCER, SNATCHER, LEECHER
+};
+
+enum MagicPower {
+	STUN, SILENCE, SNATCH, LEECH
+};
+
+enum ItemType {
+	PISTOL_AMMO,
+	STRENGTH,
+	HEALTH,
+	TELEPORT,
+	BOMB,
+	GOD_MODE,
+	SPEED,
+	TEMPLE_HEALER,
+	ATTACK_RANGE
+};
+
+enum TeamName {
+	ANGELS, DEMONS
+};
+
+struct Item {
+	ItemType item_type;
+	int ItemUsetimer;
+
+	// Used for one-time-items (like maybe Strength - don't want to have again)
+	// Low priority, remove if not required
+	bool isOneTimeItem;
+	bool isDisplayed;
+};
+
+struct Temple {
+	TeamName team_name;
+	int health;
+};
+
+struct Hero {
+	TeamName team_name;
+	//string pic_path;
+
+	int health;
+
+	int pistol_ammo;
+
+	MagicPower magic_power;
+	int magicSpellTime;
+
+	//TODO - pending item attributes
+
+};
+
