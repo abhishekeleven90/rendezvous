1165dff13e2b1b0a03e9e35ba653af657a81366d
abhiagar90 <abhiagar90@gmail.com>
2014-10-21 21:15:11 +0530

    adding an generic event sound callback

--- game/SoundAll.h
+++ game/SoundAll.h
@@ -2,42 +2,27 @@
 
 using namespace sf;
 
-//have to be global
-Sound mainSound;
-SoundBuffer mainBuffer;
-
-//whenever an event happens, can use these
-Sound eventSound;
-SoundBuffer eventBuffer;
-
 Music music;
-
+Music eventMusic;
 
 void startMainSound() {
 	int loaded = 1;
 
-	//TODO gives error(writes on cmd line) if no file
-	/*if (!mainBuffer.loadFromFile("data/sounds/pacman.wav"))
-		loaded = -1;
-
-	if (loaded) {
-		//play now
-		mainSound.setBuffer(mainBuffer);
-		mainSound.setLoop(true);
-		mainSound.play();
-
-		//no need of while loop as glutMainLoop doesn't return
-		while(1);
-
-	}*/
-
 	if (!music.openFromFile("data/sounds/mario_theme.ogg"))
-	    loaded=-1; // error
-	if(loaded)
-	{
+		loaded = -1; // error
+	if (loaded) {
 		music.setLoop(true);
 		music.play();
-		music.setLoop(true);
 		//while(1);
+	}
+}
+
+void playEventSound() {
+
+	int loaded = 1;
+	if (!eventMusic.openFromFile("data/sounds/event.wav"))
+		loaded = -1; // error
+	if (loaded) {
+		eventMusic.play();
 	}
 }
