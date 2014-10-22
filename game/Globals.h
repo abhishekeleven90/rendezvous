fa79cc17bdc0234991a79c9a2546cb8a682223be
Harinder Sethi <sethi.harinder@gmail.com>
2014-10-22 18:37:55 +0530

    changes with headers.

---
+++ game/Globals.h
@@ -0,0 +1,23 @@
+//TextureIds
+GLuint grass_texId;
+GLuint spawn_texId;
+GLuint war_texId;
+
+GLuint attribute_bg_texId;
+
+GLuint tree_texId;
+GLuint stone_texId;
+
+GLuint t_angels_texId; //temples
+GLuint t_demons_texId;
+
+GLuint h_disabler_texId;
+GLuint h_slower_texId; //hero_mario
+GLuint h_burster_texId; //hero_soldier
+GLuint h_stunner_texId;
+
+GLuint i_movSpeed_texId;
+GLuint i_attackSpeed_texId;
+GLuint i_damage_texId;
+GLuint i_health_texId;
+
