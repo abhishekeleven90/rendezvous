8cf733f770e6736f2e63b479af73f1d62c77d7e3
Harinder Sethi <sethi.harinder@gmail.com>
2014-10-31 21:39:55 +0530

    new file

---
+++ game/FilePaths_att.h
@@ -0,0 +1,34 @@
+#ifndef FILE_PATHS_ATT_H
+#define FILE_PATHS_ATT_H
+
+#define PATH_IMG_ATT_CURSE_BURST "data/images/att_curse_burst.bmp"
+#define PATH_IMG_ATT_CURSE_DISABLE "data/images/att_curse_disable.bmp"
+#define PATH_IMG_ATT_CURSE_SLOW "data/images/att_curse_slow.bmp"
+#define PATH_IMG_ATT_CURSE_STUN "data/images/att_curse_stun.bmp"
+#define PATH_IMG_ATT_CURSE_NONE "data/images/att_curse_none.bmp"
+
+#define PATH_IMG_ATT_HERO_BURSTER "data/images/att_hero_burster.bmp"
+#define PATH_IMG_ATT_HERO_DISABLER "data/images/att_hero_disabler.bmp"
+#define PATH_IMG_ATT_HERO_SLOWER "data/images/att_hero_slower.bmp"
+#define PATH_IMG_ATT_HERO_STUNNER "data/images/att_hero_stunner.bmp"
+
+#define PATH_IMG_ATT_MODE_BASIC "data/images/att_mode_basic.bmp"
+#define PATH_IMG_ATT_MODE_MAGIC "data/images/att_mode_magic.bmp"
+
+#define PATH_IMG_ATT_TEAM_ANGELS "data/images/att_team_angels.bmp"
+//#define PATH_IMG_ATT_TEAM_DEMONS "data/images/att_team_demons.bmp"
+
+#define PATH_IMG_ATT_TEAM_ENEMY "data/images/att_team_enemy.bmp"
+#define PATH_IMG_ATT_TEAM_MY "data/images/att_team_my.bmp"
+
+#define PATH_IMG_ATT_TIMERS "data/images/att_timers.bmp"
+#define PATH_IMG_ATT_TIMERS_ITEM_OFF "data/images/att_timers_itemOff.bmp"
+#define PATH_IMG_ATT_TIMERS_ITEM_ON "data/images/att_timers_itemOn.bmp"
+#define PATH_IMG_ATT_TIMERS_MAGIC_OFF "data/images/att_timers_magicOff.bmp"
+#define PATH_IMG_ATT_TIMERS_MAGIC_ON "data/images/att_timers_magicOn.bmp"
+
+#define PATH_IMG_ATT_WALL "data/images/att_wall.bmp"
+
+#define PATH_IMG_ATT_HEALTH "data/images/att_health.bmp"
+
+#endif
